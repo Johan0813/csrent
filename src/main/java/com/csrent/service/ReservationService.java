f93b66cd5326c363f998141c9f3f1996fca0d878
Johan <johan131105@gmail.com>
2025-06-28 16:35:47 -0600

    problema con reservation...

--- src/main/java/com/csrent/service/ReservationService.java
+++ src/main/java/com/csrent/service/ReservationService.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationService {
@@ -60,6 +61,9 @@ public class ReservationService {
     }
     public void deleteById(Integer id) {
         reservationRepository.deleteById(id);
+    }
+    public Optional<Reservation> findById(Integer id) {
+        return reservationRepository.findById(id);
     }
 
 }
